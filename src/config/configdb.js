f0e6767bfb46aa9ac3f2498105c1d737f95aa5a2
Khanh <Quockhanh6310@gmail.com>
2026-05-04 13:07:33 +0700

    Initial commit

---
+++ src/config/configdb.js
@@ -0,0 +1,19 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('node_fulltask', 'root', '123456', {
+  host: '127.0.0.1',
+  dialect: 'mysql',
+  logging: false
+});
+
+let connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+// Sửa dòng này:
+export default connectDB;
